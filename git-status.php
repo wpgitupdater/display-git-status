da8df4de5a4c52a9ec7b9e0908b5397d2b169e5c
Lee Mason <lee.mason@gambitnash.co.uk>
2020-12-18 11:18:41 +0000

    feature(admin): Add options for repository location

--- git-status.php
+++ git-status.php
@@ -34,13 +34,36 @@ if ( ! is_admin() ) {
 	return;
 }
 
+register_activation_hook( __FILE__, 'get_status_install_hook' );
+/**
+ * Sets default option values if none are present.
+ */
+function get_status_install_hook() {
+	if ( ! get_option( 'git_status_options' ) ) {
+		update_option( 'git_status_options', array( 'git_directory' => rtrim( WP_CONTENT_DIR, '/' ) ) );
+	}
+}
+
+/**
+ * Returns the location of the git repository, defaulting to `wp-content` if not set.
+ *
+ * @return string The location of the git repository
+ */
+function git_status_get_respository_location() {
+	$options = get_option( 'git_status_options' );
+	if ( is_array( $options ) && isset( $options['git_directory'] ) ) {
+		return $options['git_directory'];
+	}
+	return rtrim( WP_CONTENT_DIR, '/' );
+}
+
 /**
  * Returns the current branch name for a given location.
  *
  * @return string The current branch name
  */
 function git_status_get_branch_name() {
-	return trim( shell_exec( 'cd ' . __DIR__ . ' && git rev-parse --abbrev-ref HEAD' ) );
+	return trim( shell_exec( 'cd ' . git_status_get_respository_location() . ' && git rev-parse --abbrev-ref HEAD' ) );
 }
 
 /**
@@ -49,7 +72,7 @@ function git_status_get_branch_name() {
  * @return bool true when no untracked changes, false otherwise.
  */
 function git_status_is_up_to_date() {
-	$status = trim( shell_exec( 'cd ' . __DIR__ . ' && git status --porcelain=v1' ) );
+	$status = trim( shell_exec( 'cd ' . git_status_get_respository_location() . ' && git status --porcelain=v1' ) );
 	if ( '' === $status ) {
 		return true;
 	}
@@ -66,6 +89,7 @@ function git_status_admin_css() {
 	.git-status-menu img.ab-icon {
 		height: 22px !important;
 		width: 22px !important;
+		padding-top: 5px !important;
 	}
 	.git-status-menu.git-status-untracked a {
 		background-color: #f05133 !important;
@@ -79,7 +103,7 @@ function git_status_admin_css() {
 
 add_action( 'admin_bar_menu', 'git_status_add_branch_link', 100 );
 /**
- * Adds a link with the current branch to the admin bar
+ * Adds a link with the current branch to the admin bar when repository located
  *
  * @param WP_Admin_Bar $admin_bar WordPress admin bar instance.
  */
@@ -88,6 +112,10 @@ function git_status_add_branch_link( WP_Admin_Bar $admin_bar ) {
 		return;
 	}
 	$branch = git_status_get_branch_name();
+	if ( '' === $branch ) {
+		return;
+	}
+
 	if ( git_status_is_up_to_date() ) {
 		$img = plugins_url( 'assets/git.svg', __FILE__ );
 		$title = sprintf(
@@ -118,4 +146,87 @@ function git_status_add_branch_link( WP_Admin_Bar $admin_bar ) {
 			),
 		)
 	);
+}
+
+add_action( 'admin_menu', 'git_status_add_pages' );
+/**
+ * Adds the Git Status Tools menu page.
+ */
+function git_status_add_pages() {
+	add_management_page( __( 'Git Status', 'git-status' ), __( 'Git Status', 'git-status' ), 'manage_options', 'git-status', 'git_status_page' );
+}
+
+/**
+ * Outputs the Git Status page content.
+ */
+function git_status_page() {
+	if ( git_status_get_branch_name() === '' ) {
+		add_settings_error( 'git_status_options', 'git_status_setting_git_directory', __( 'The saved location is not a git repository! The git status menu item will be hidden from view.', 'git-status' ), 'error' );
+	}
+	?>
+	<div class="wrap">
+		<style type="text/css">
+			.page-title img {
+				width: 30px;
+				height: 30px;
+				margin-bottom: -6px;
+			}
+		</style>
+		<h1 class="page-title">
+			<img src="<?php echo esc_attr( plugins_url( 'assets/git.svg', __FILE__ ) ); ?>" alt="<?php esc_attr_e( 'Git Icon', 'git-status' ); ?>" />
+			<?php esc_attr_e( 'Git Status', 'git-status' ); ?>
+		</h1>
+		<form action="options.php" method="post">
+			<?php
+			settings_errors( 'git_status_options' );
+			settings_fields( 'git_status_options' );
+			do_settings_sections( 'git_status' );
+			?>
+			<input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save Settings', 'git-status' ); ?>" />
+		</form>
+	</div>
+	<?php
+}
+
+add_action( 'admin_init', 'git_status_register_settings' );
+/**
+ * Register our plugins settings, sections and fields.
+ */
+function git_status_register_settings() {
+	register_setting(
+		'git_status_options',
+		'git_status_options',
+		array(
+			'type' => 'array',
+			'sanitize_callback' => 'git_status_sanitize_options',
+		)
+	);
+	add_settings_section( 'git_settings', __( 'Git Settings', 'git-status' ), 'git_status_git_section_text', 'git_status' );
+	add_settings_field( 'git_status_setting_git_directory', __( 'Git Repository Location', 'git-status' ), 'git_status_setting_git_directory', 'git_status', 'git_settings', array( 'label_for' => 'git_status_setting_git_directory' ) );
+}
+
+/**
+ * Sanitize user supplied settings for our plugin, adding notices where appropriate.
+ *
+ * @param array $options plugin settings form options.
+ * @return array Sanitized settings
+ */
+function git_status_sanitize_options( $options ) {
+	$options['git_directory'] = esc_attr( rtrim( $options['git_directory'], '/' ) );
+	add_settings_error( 'git_status_options', 'git_status_setting_git_directory', __( 'Settings Saved', 'git-status' ), 'success' );
+	return $options;
+}
+
+/**
+ * Introduction text for the git settings section.
+ */
+function git_status_git_section_text() { }
+
+/**
+ * Output our git directory setting input.
+ */
+function git_status_setting_git_directory() {
+	$options = get_option( 'git_status_options' );
+	echo '<input id="git_status_setting_git_directory" class="regular-text code" name="git_status_options[git_directory]" type="text" value="' . esc_attr( $options['git_directory'] ) . '" />';
+	echo '<p class="description">' . esc_attr( 'Enter the full path to your sites git repository.', 'git-status' ) . '</p>';
 }
