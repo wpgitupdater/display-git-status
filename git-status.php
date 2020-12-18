a9e8a6d6eb9a352bae61b3df7cd0487398025efd
Lee Mason <lee.mason@gambitnash.co.uk>
2020-12-18 00:03:52 +0000

    feature(menu): Highlight untracked state with highlighted background for menu link

--- git-status.php
+++ git-status.php
@@ -43,6 +43,40 @@ function git_status_get_branch_name() {
 	return trim( shell_exec( 'cd ' . __DIR__ . ' && git rev-parse --abbrev-ref HEAD' ) );
 }
 
+/**
+ * Returns a boolean for git status, true being up to date, false otherwise
+ *
+ * @return bool true when no untracked changes, false otherwise.
+ */
+function git_status_is_up_to_date() {
+	$status = trim( shell_exec( 'cd ' . __DIR__ . ' && git status --porcelain=v1' ) );
+	if ( '' === $status ) {
+		return true;
+	}
+
+	return false;
+}
+
+add_action( 'admin_head', 'git_status_admin_css' );
+/**
+ * Add plugins admin css.
+ */
+function git_status_admin_css() {
+	echo '<style>
+	.git-status-menu img.ab-icon {
+		height: 22px !important;
+		width: 22px !important;
+	}
+	.git-status-menu.git-status-untracked a {
+		background-color: #f05133 !important;
+	}
+	.git-status-menu.git-status-untracked:hover a {
+		background-color: #d4492f !important;
+		color: #ffffff !important;
+	}
+</style>';
+}
+
 add_action( 'admin_bar_menu', 'git_status_add_branch_link', 100 );
 /**
  * Adds a link with the current branch to the admin bar
@@ -54,19 +88,33 @@ function git_status_add_branch_link( WP_Admin_Bar $admin_bar ) {
 		return;
 	}
 	$branch = git_status_get_branch_name();
+	if ( git_status_is_up_to_date() ) {
+		$img = plugins_url( 'assets/git.svg', __FILE__ );
+		$title = sprintf(
+			/* translators: Asserting the current git branch */
+			__( 'You are currently on the %s branch', 'git-status' ),
+			$branch
+		);
+		$class_names = 'git-status-menu git-status-up-to-date';
+	} else {
+		$img = plugins_url( 'assets/git-white.svg', __FILE__ );
+		$title = sprintf(
+			/* translators: Asserting the current git branch */
+			__( 'You are currently on the %s branch, but there are uncommitted changes!', 'git-status' ),
+			$branch
+		);
+		$class_names = 'git-status-menu git-status-untracked';
+	}
 	$admin_bar->add_menu(
 		array(
 			'id'    => 'git-status',
 			'parent' => null,
 			'group'  => null,
-			'title' => '<img src="' . plugins_url( 'assets/git.svg', __FILE__ ) . '" alt="' . __( 'Git Icon', 'git-status' ) . '" style="height: 22px;width: 22px;" class="ab-icon" />' . $branch,
+			'title' => '<img src="' . $img . '" alt="' . __( 'Git Icon', 'git-status' ) . '" class="ab-icon" />' . $branch,
 			'href'  => admin_url( 'admin.php?page=git-status' ),
 			'meta' => array(
-				'title' => sprintf(
-					/* translators: Asserting the current git branch */
-					__( 'You are currently on the %s branch', 'git-status' ),
-					$branch
-				),
+				'title' => $title,
+				'class' => $class_names,
 			),
 		)
 	);
